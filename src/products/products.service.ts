cce41ae89e6e38209c70271f4b16a4994d35c643
Pedro Santana <pedromanu27@gmail.com>
2023-10-30 17:30:37 -0300

    prisma models e migrations

---
+++ src/products/products.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { DatabaseService } from 'src/database/database.service';
+
+@Injectable()
+export class ProductsService {
+  constructor(private readonly databaseService: DatabaseService) {}
+
+  async create(createProductDto: Prisma.ProductCreateInput) {
+    return this.databaseService.product.create({ data: createProductDto });
+  }
+
+  findAll() {
+    return this.databaseService.product.findMany({});
+  }
+
+  async findOne(id: number) {
+    return this.databaseService.product.findUnique({ where: { id } });
+  }
+
+  update(id: number, updateProductDto: Prisma.ProductUpdateInput) {
+    return this.databaseService.product.update({
+      where: {
+        id,
+      },
+      data: updateProductDto,
+    });
+  }
+
+  remove(id: number) {
+    return this.databaseService.product.delete({
+      where: { id },
+    });
+  }
+}
